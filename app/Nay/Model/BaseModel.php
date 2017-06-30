e84f54c96b17bbdbab42a3b68c247814580932aa
Alfredo Braga <alphabraga@hotmail.com>
2017-06-29 22:16:49 -0300

    Base model agora tambem manipula as imagens

--- app/Nay/Model/BaseModel.php
+++ app/Nay/Model/BaseModel.php
@@ -45,4 +45,35 @@ class BaseModel extends Model
             return ['afectedRows' => null, 'error' => $e->getCode(), 'exception' => $e];
         }
     }
+
+
+    public function getImageFileName($withPath = true, $withNumber = true, $withExtension = true)
+    {
+        if($withNumber)
+        {
+            $savedImages = $this->getImages();
+
+            $numberOfSavedImages = count($savedImages);
+
+            $nextNumberOfImage = $numberOfSavedImages++;
+        }
+
+        return (($withPath)? $this->imagePath : '') . $this->id . '-' . $this->slug . '-' . (($withNumber)? $nextNumberOfImage: '') . (($withExtension)? '.jpg': '');
+    }
+
+    public function saveFakeImage(\Faker\Generator $faker)
+    {
+        $imageFileName = $this->getIMageFileName(true, true, true);
+
+        file_put_contents($imageFileName, file_get_contents($faker->imageUrl(255, 255)));
+    }
+
+    public function getImages()
+    {
+        //get images witout number and without extension
+        $images = glob($this->getImageFileName(true, false, false) . '*');
+
+        return $images;
+    }
+
 }
