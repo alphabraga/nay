0b9532eecf4a1a988fe3bfae3b2277e58f3338f2
Alfredo Braga <alphabraga@hotmail.com>
2017-07-02 15:58:22 -0300

    Usando o admin-lte Usando o yarn no projeto Fazendo a tela de login no sistema

---
+++ resources/views/includes/painel.blade.php
@@ -0,0 +1,1 @@
+painel.blade.php
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/pagina', 'HomeController@pagina');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
