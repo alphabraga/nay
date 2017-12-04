70b04dafe52d0fa7f0c822f8defb3023d9364845
Alfredo Braga <alfredo.braga@emap.ma.gov.br>
2017-12-04 14:26:02 -0300

    colcoando o nome da tabela

--- app/User.php
+++ app/User.php
@@ -10,6 +10,13 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * The 
+     *
+     * @var array
+     */
+    protected $table = 'users';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
