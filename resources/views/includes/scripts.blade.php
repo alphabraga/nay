10163b859bdb7961a8e7b092d1523101654a628a
Alfredo Braga <alphabraga@hotmail.com>
2018-01-23 15:25:06 -0300

    agora o sistema roda 100 local não pega nenhum recurso de fora do servidor...

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -13,25 +13,15 @@
 <script src="{{URL::to('packages/node_modules/admin-lte/dist/js/app.min.js')}}"></script>
 
 <script src="{{URL::to('packages/node_modules/bootbox/bootbox.min.js')}}"></script>
-
 <script type="text/javascript" src="{{URL::to('packages/node_modules/jquery.maskedinput/src/jquery.maskedinput.js')}}"></script>
-
 <script type="text/javascript" src="{{URL::to('packages/node_modules/jquery-maskmoney/src/jquery.maskMoney.js')}}"></script>
-
 <script type="text/javascript" src="{{URL::to('packages/node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
 <script type="text/javascript" src="{{URL::to('packages/node_modules/bootstrap-datepicker/dist/locales/bootstrap-datepicker.pt-BR.min.js')}}"></script>
-
 <script type="text/javascript" src="{{URL::to('packages/node_modules/admin-lte/plugins/datatables/jquery.dataTables.min.js')}}" ></script>
-
 <script type="text/javascript" src="{{URL::to('packages/node_modules/admin-lte/plugins/datatables/dataTables.bootstrap.min.js')}}" ></script>
-
 <script type="text/javascript" src="{{URL::to('packages/node_modules/admin-lte/plugins/select2/select2.js')}}" ></script>
-
 {{-- <script type="text/javascript" src="{{URL::to('packages/node_modules/angular/angular.min.js')}}" ></script> --}}
-
-
 <!-- AdminLTE for demo purposes -->
 <script src="{{URL::to('packages/node_modules/admin-lte/dist/js/demo.js')}}"></script>
-
 <!-- AdminLTE for demo purposes -->
 <script src="{{URL::to('/js/application.js')}}"></script>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,11 +8,14 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
+
+  <link rel="stylesheet" type="text/css" href="{{URL::to('css/googlefonts.css')}}">
+
   <link rel="stylesheet" href="{{URL::to('packages/node_modules/admin-lte/bootstrap/css/bootstrap.min.css')}}">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="{{URL::to('packages/node_modules/font-awesome/css/font-awesome.min.css')}}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="{{URL::to('packages/node_modules/ionicons/css/ionicons.min.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{URL::to('packages/node_modules/admin-lte/dist/css/AdminLTE.min.css')}}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
